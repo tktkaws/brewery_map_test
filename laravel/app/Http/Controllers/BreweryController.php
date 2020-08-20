fe2df44cb4980437ca6728640708e09cf7aea3e0
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-21 07:32:09 +0900

    create brewery_model

--- laravel/app/Http/Controllers/BreweryController.php
+++ laravel/app/Http/Controllers/BreweryController.php
@@ -2,48 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Brewery;
 use Illuminate\Http\Request;
 
 class BreweryController extends Controller
 {
-        //==========ここから追加==========
-        public function index()
-        {
-            // ダミーデータ
-            $breweries = [
-                (object) [
-                    'id' => 1,
-                    'title' => 'タイトル1',
-                    'body' => '本文1',
-                    'created_at' => now(),
-                    'user' => (object) [
-                        'id' => 1,
-                        'name' => 'ユーザー名1',
-                    ],
-                ],
-                (object) [
-                    'id' => 2,
-                    'title' => 'タイトル2',
-                    'body' => '本文2',
-                    'created_at' => now(),
-                    'user' => (object) [
-                        'id' => 2,
-                        'name' => 'ユーザー名2',
-                    ],
-                ],
-                (object) [
-                    'id' => 3,
-                    'title' => 'タイトル3',
-                    'body' => '本文3',
-                    'created_at' => now(),
-                    'user' => (object) [
-                        'id' => 3,
-                        'name' => 'ユーザー名3',
-                    ],
-                ],
-            ];
-
-            return view('breweries.index', ['breweries' => $breweries]);
-        }
-        //==========ここまで追加==========
+    public function index()
+    {
+        $breweries = Brewery::all()->sortByDesc('created_at');
+        return view('breweries.index', ['breweries' => $breweries]);
+    }
 }
