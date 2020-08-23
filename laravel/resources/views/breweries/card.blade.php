a2f190a272a62b1c9d0f13a9996f79d158250474
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-23 15:34:20 +0900

    8-8 タグの表示

--- laravel/resources/views/breweries/card.blade.php
+++ laravel/resources/views/breweries/card.blade.php
@@ -74,4 +74,20 @@
         </div>
     </div>
     {{--ここまで追加--}}
+    {{--------ここから追加--------}}
+    @foreach($brewery->tags as $tag)
+    @if($loop->first)
+    <div class="card-body pt-0 pb-4 pl-3">
+        <div class="card-text line-height">
+            @endif
+            <a href="" class="border p-1 mr-1 mt-1 text-muted">
+                {{ $tag->name }}
+            </a>
+            @if($loop->last)
+        </div>
+    </div>
+    @endif
+    @endforeach
+    {{--------ここまで追加--------}}
+
 </div>
