2e82d9f4359628c1258307e7f2ba09ce85695d96
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-23 13:14:24 +0900

    7-5 change color of heart_icon

--- laravel/resources/views/breweries/card.blade.php
+++ laravel/resources/views/breweries/card.blade.php
@@ -67,7 +67,7 @@
     {{--ここから追加--}}
     <div class="card-body pt-0 pb-2 pl-3">
         <div class="card-text">
-            <brewery-like>
+            <brewery-like :initial-is-liked-by='@json($brewery->isLikedBy(Auth::user()))'>
             </brewery-like>
         </div>
     </div>
