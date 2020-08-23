4da0518416bd47bf00b20ddf7db7ff60fbbdd717
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-23 15:56:11 +0900

    8-11 タグにハッシュタグを追加

--- laravel/resources/views/breweries/card.blade.php
+++ laravel/resources/views/breweries/card.blade.php
@@ -81,7 +81,8 @@
         <div class="card-text line-height">
             @endif
             <a href="" class="border p-1 mr-1 mt-1 text-muted">
-                {{ $tag->name }}
+                {{ $tag->hashtag }} {{----------この行を変更----------}}
+
             </a>
             @if($loop->last)
         </div>
