acbd8f7570346afeec9e37f03cfde13391341aab
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-24 09:14:07 +0900

    9-4 フォローコンポーネント、フォローテーブル作成

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -2,11 +2,13 @@ import "./bootstrap";
 import Vue from "vue";
 import BreweryLike from "./components/BreweryLike";
 import BreweryTagsInput from "./components/BreweryTagsInput";
+import FollowButton from "./components/FollowButton";
 
 const app = new Vue({
     el: "#app",
     components: {
         BreweryLike,
-        BreweryTagsInput
+        BreweryTagsInput,
+        FollowButton
     }
 });
