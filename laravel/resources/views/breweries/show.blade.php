52b315e4301eff73e079ede9ebdfc46d731021c1
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-25 14:22:24 +0900

    fix apikey

--- laravel/resources/views/breweries/show.blade.php
+++ laravel/resources/views/breweries/show.blade.php
@@ -8,8 +8,7 @@
     @include('breweries.card')
 
     <div id="map-container-google-1" class="z-depth-1-half map-container">
-        <iframe
-            src='https://www.google.com/maps/embed/v1/search?key=AIzaSyBrHF1_NfV2WYVdh0PoZKD3qf52MXJwEx8&q={{ $brewery->name }}'
+        <iframe src='https://www.google.com/maps/embed/v1/search?key={{ env('GOOGLE_MAP_API') }}&q={{ $brewery->name }}'
             width='100%' height='480' frameborder='0' style="border:0">
         </iframe>
     </div>
